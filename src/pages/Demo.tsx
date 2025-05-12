6f4149d0ba51aaae1002603f8a20e20024b8b6f5
IngSeb0 <ls.contreras@uniandes.edu.co>
2025-05-11 23:01:48 -0500

    por favoor

--- src/pages/Demo.tsx
+++ src/pages/Demo.tsx
@@ -489,17 +489,14 @@ const animationActiveRef = useRef(false);
     navigate('/');
   };
 useEffect(() => {
-  if (!mixer || !scene) return;
 
   if (conversation.isSpeaking) {
     mixer.timeScale = 1;
-    animationActiveRef.current = true;
 
     
     
   } else {
     mixer.timeScale = 0;
-    animationActiveRef.current = false;
   }
 }, [conversation.isSpeaking, mixer, scene]);
   if (loading) {
